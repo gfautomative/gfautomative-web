2a2b5954596a7f94b665a217ee685e16a4299e7d
gfautomative <gfautomative@gmail.com>
2026-09-03 18:44:46 +0400

    Update modern web build for iOS

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "449640034" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1096532607" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
