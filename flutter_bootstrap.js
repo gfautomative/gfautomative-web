d485e4c2f53c9728d4613aa8a6e8508ea4a845fe
gfautomative <gfautomative@gmail.com>
2026-09-11 22:19:18 +0400

    تحديث موديول الكرت السريع 3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "988799100" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4026308378" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
